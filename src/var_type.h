32f75b88ad6959b4a606ef98a41e5fdfdcb683b9
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-12 21:14:07 +0200

    var_t header

---
+++ src/var_type.h
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the MIL License
+ *******************************************************************************
+ *
+ * Filename: var_type.h
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2016-05-12   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef VAR_TYPE_H_
+#define VAR_TYPE_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+typedef uint32_t var_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VAR_TYPE_H_ */
