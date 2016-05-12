a7f9b2fd6e8392b5ddd16d3b1b9eac739b11abbb
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-12 21:14:28 +0200

    SPAN(var_t) header

---
+++ src/span_var.h
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the MIT License
+ *******************************************************************************
+ *
+ * Filename: span_var.h
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2016-05-12   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef SPAN_VAR_H_
+#define SPAN_VAR_H_
+
+#include "var_type.h"
+#include "span.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+DEFINE_SPAN(var_t);
+DEFINE_SPAN_T(var_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPAN_VAR_H_ */
