8fa2d42c639d7de551b13b99919f9c2772e90bc4
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-12 21:15:32 +0200

    DEFINE_PAIR_T

--- src/pair.h
+++ src/pair.h
@@ -34,6 +34,7 @@ extern "C"
 
 
 #define PAIR(_Tp1, _Tp2) struct STR_CONCAT5(pair_, _Tp1, _, _Tp2, _s)
+#define PAIR_T(_Tp1, _Tp2) STR_CONCAT5(pair_, _Tp1, _, _Tp2, _t)
 
 #define DEFINE_PAIR(_Tp1, _Tp2) \
 PAIR(_Tp1, _Tp2) \
@@ -41,6 +42,8 @@ PAIR(_Tp1, _Tp2) \
     _Tp1 first; \
     _Tp2 second; \
 }
+
+#define DEFINE_PAIR_T(_Tp1, _Tp2) typedef PAIR(_Tp1, _Tp2) PAIR_T(_Tp1, _Tp2) __attribute__ ((unused))
 
 #define MAKE_PAIR(_Tp1, _Tp2, p, q) (PAIR(_Tp1, _Tp2)){p, q}
 
