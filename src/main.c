7b27045bed02a247dde8edf6302c7756bffb1de2
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-15 23:58:57 +0200

    removed dead code

--- src/main.c
+++ src/main.c
@@ -10,24 +10,11 @@
 #include "pair.h"
 #include "work.h"
 
-//#include "run.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-
-//int init_options(option_t *opt)
-//{
-//    opt->n = 0;
-//    opt->k = 0;
-//    opt->b = 0;
-//    opt->s_std = 0.0;
-//    opt->s_option = 0;
-//
-//    return 0;
-//}
 
 int main(int argc, char const * argv[])
 {
@@ -50,48 +37,6 @@ int main(int argc, char const * argv[])
     }
 
     fprintf(stderr, "Finished Succesfully.\n");
-
-    return 0;
-
-    ////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-//    int err;
-//    option_t opt;
-//
-//    err = init_options(&opt);
-//
-//    if (err < 0)
-//    {
-//        fprintf(stderr, "Error initializing arguments.\n");
-//        return 1;
-//    }
-//
-//    err = parse_args(argc, argv, &opt);
-//    if (err < 0)
-//    {
-//        return 1;
-//    }
-//
-////    const time_t ltime = time(NULL);
-////    fprintf(stderr, "%s", asctime(localtime(&ltime)));
-//
-//    err = run(&opt);
-//    if (err < 0)
-//    {
-//        fprintf(stderr, "Error running.\n");
-//        return 1;
-//    }
-//
-//    fprintf(stderr, "Finished Succesfully.\n");
 
     return 0;
 }
