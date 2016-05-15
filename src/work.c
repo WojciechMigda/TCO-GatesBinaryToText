d55ef7f91bca4d118e59d772693dc8855885dbe2
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-15 23:09:00 +0200

    threaded jobs

--- src/work.c
+++ src/work.c
@@ -46,6 +46,103 @@ DEFINE_PAIR(SPAN_T(var_t), SPAN_T(indexed_score_t));
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <pthread.h>
+
+
+typedef void * (*thread_fn)(void *);
+
+typedef struct
+{
+    SPAN(indexed_score_t) indexed_scores;
+    SPAN(var_t) tuples;
+    size_t tup_dim;
+    size_t n_tup;
+} thread_out12_ctx_t;
+
+
+static
+void * thread_out1(void * p)
+{
+    thread_out12_ctx_t * ctx = (thread_out12_ctx_t *)p;
+
+    out1(
+        ctx->indexed_scores,
+        ctx->tuples,
+        ctx->tup_dim,
+        ctx->n_tup,
+        0);
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+
+static
+void * thread_out2(void * p)
+{
+    thread_out12_ctx_t * ctx = (thread_out12_ctx_t *)p;
+
+    out2(
+        ctx->indexed_scores,
+        ctx->tuples,
+        ctx->tup_dim,
+        ctx->n_tup,
+        0);
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+
+typedef struct
+{
+    SPAN(indexed_score_t) indexed_scores;
+    SPAN(var_t) tuples;
+    SPAN(deque_t) var_to_tupix;
+    size_t tup_dim;
+    uint32_t k;
+} thread_out3_ctx_t;
+
+static
+void * thread_out3(void * p)
+{
+    thread_out3_ctx_t * ctx = (thread_out3_ctx_t *)p;
+
+    build_var_to_tupix(
+        ctx->var_to_tupix,
+        ctx->indexed_scores,
+        ctx->tuples,
+        ctx->tup_dim);
+
+    out3(
+        ctx->indexed_scores,
+        ctx->tuples,
+        ctx->var_to_tupix,
+        ctx->tup_dim,
+        ctx->k,
+        0);
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+
+typedef struct
+{
+    SPAN(indexed_score_t) indexed_scores;
+    double b;
+} thread_out4_ctx_t;
+
+static
+void * thread_out4(void * p)
+{
+    thread_out4_ctx_t * ctx = (thread_out4_ctx_t *)p;
+
+    out4(ctx->indexed_scores, ctx->b, 0);
+
+    pthread_exit(NULL);
+    return NULL;
+}
 
 
 int work(const struct program_options_s * program_options_p)
@@ -95,36 +192,63 @@ int work(const struct program_options_s * program_options_p)
             //, var_to_tupix
             );
 
-    /* we need this for average scores per variable */
-    build_var_to_tupix(
-        var_to_tupix,
-        tuples_and_indexed_score.second,
-        tuples_and_indexed_score.first,
-        data_ctx.d);
 
-    out1(
-        tuples_and_indexed_score.second,
-        tuples_and_indexed_score.first,
-        data_ctx.d,
-        program_options_p->n,
-        program_options_p->nthreads);
 
-    out2(
+
+    pthread_t thread[4];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    thread_out12_ctx_t thread_out12_ctx =
+    {
         tuples_and_indexed_score.second,
         tuples_and_indexed_score.first,
         data_ctx.d,
-        program_options_p->n,
-        program_options_p->nthreads);
+        program_options_p->n
+    };
 
-    out3(
+    thread_out3_ctx_t thread_out3_ctx =
+    {
         tuples_and_indexed_score.second,
         tuples_and_indexed_score.first,
         var_to_tupix,
         data_ctx.d,
-        program_options_p->k,
-        program_options_p->nthreads);
+        program_options_p->k
+    };
 
-    out4(tuples_and_indexed_score.second, program_options_p->b, program_options_p->nthreads);
+    thread_out4_ctx_t thread_out4_ctx =
+    {
+        tuples_and_indexed_score.second,
+        program_options_p->b
+    };
+
+    thread_fn thread_fns[] =
+    {
+        thread_out1,
+        thread_out2,
+        thread_out3,
+        thread_out4
+    };
+    void * thread_cxts[] =
+    {
+        &thread_out12_ctx,
+        &thread_out12_ctx,
+        &thread_out3_ctx,
+        &thread_out4_ctx
+    };
+
+    size_t thix = 0;
+    for (thix = 0; thix < sizeof (thread_fns) / sizeof (thread_fns[0]); ++thix)
+    {
+        const int rc = pthread_create(&thread[thix], &attr, thread_fns[thix], thread_cxts[thix]);
+        if (rc)
+        {
+            fprintf(stderr, "[work] pthread_create error: %d, ignoring\n", rc);
+        }
+    }
+
+
 
     if (OPTION_TYPE_IS_SOME(program_options_p->maybe_s))
     {
@@ -137,6 +261,19 @@ int work(const struct program_options_s * program_options_p)
             OPTION_TYPE_VALUE(program_options_p->maybe_s),
             program_options_p->nthreads);
     }
+
+
+    for (thix = 0; thix < sizeof (thread_fns) / sizeof (thread_fns[0]); ++thix)
+    {
+        void * status = NULL;
+
+        const int rc = pthread_join(thread[thix], &status);
+        if (rc)
+        {
+            fprintf(stderr, "[work] pthread_join error: %d, ignoring\n",rc);
+        }
+    }
+
 
     free(tuples_and_indexed_score.first.ptr);
     free(tuples_and_indexed_score.second.ptr);
