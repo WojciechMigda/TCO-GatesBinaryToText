bd9a04fbd469b81273be5c6646c8883adb9d0d4e
Wojciech Migda <wojtek.golf@interia.pl>
2016-05-15 21:02:18 +0200

    work with gzipped reference files

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # diffs should run clean
-make
-./run -s 0 test/d2/in1.bin test/d2/in2.bin
-diff <(sort --parallel=4 out5.txt) <(sort --parallel=4 test/d2/out5.txt)
-#diff out5.txt test/d2/out5.txt
-./run -s 0 test/d3/in1.bin test/d3/in2.bin
-diff <(sort --parallel=4 out5.txt) <(sort --parallel=4 test/d3/out5.txt)
-#diff out5.txt test/d3/out5.txt
-./run -s 0 test/d4/in1.bin test/d4/in2.bin
-diff <(sort --parallel=4 out5.txt) <(sort --parallel=4 test/d4/out5.txt)
-#diff out5.txt test/d4/out5.txt
+
+./run -t 4 -s 0 test/d2/in1.bin test/d2/in2.bin
+diff <(sort --parallel=4 out5.txt) <(gunzip -c test/d2/out5.txt.gz | sort --parallel=4)
+
+./run -t 4 -s 0 test/d3/in1.bin test/d3/in2.bin
+diff <(sort --parallel=4 out5.txt) <(gunzip -c test/d3/out5.txt.gz | sort --parallel=4)
+
+./run -t 4 -s 0 test/d4/in1.bin test/d4/in2.bin
+diff <(sort --parallel=4 out5.txt) <(gunzip -c test/d4/out5.txt.gz | sort --parallel=4)
+
 rm out5.txt
